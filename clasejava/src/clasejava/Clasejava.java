94b07dfe15a6749d565ff73227fea7d55bd7a100
lizbethguadalupe <lupitalizzy@gmail.com>
2021-02-05 19:31:27 -0600

    crea proyecto clase java

---
+++ clasejava/src/clasejava/Clasejava.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clasejava;
+
+/**
+ *
+ * @author Hp
+ */
+public class Clasejava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int edad;
+        edad = 10;
+        int numero = 5;
+        
+        int edades = 25;
+        int Numero = 18;
+        
+        System.out.println(edades);
+        
+    }
+    
+}
